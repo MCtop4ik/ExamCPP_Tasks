8f8085ff18f8d7edd2b5f56bc19ec29187c7a295
MCtop4ik <senya.zakharov.0808@mail.ru>
2026-01-17 14:20:43 +0300

    fix: add more statements in snippet at task 3

--- Exam_I-semester/Task3/task.cc
+++ Exam_I-semester/Task3/task.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 
-class IntArray;
+class IntArray; // std deprecated
 
 int main() {
     IntArray a(3, 0);
     std::cout << a; // 0 0 0
     a = 1 + a + 2;
     
+    std::cout << a;
+
+    a -= 0;
+
+    std::cout << a;
+
+    IntArray a2(2, 1);
+    std::cout << a2;
+
+    std::cout << a + a2;
 }
