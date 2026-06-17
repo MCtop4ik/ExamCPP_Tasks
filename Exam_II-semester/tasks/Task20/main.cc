164e9be8a0704ddc414d3768e571900edeacd43e
Senya Zak <senyaza@mail.ru>
2026-06-17 22:03:02 +0300

    fix: codestyle task19, task20

--- Exam_II-semester/tasks/Task20/main.cc
+++ Exam_II-semester/tasks/Task20/main.cc
@@ -3,8 +3,8 @@
 // solve https://godbolt.org/z/WfnK415dE
 
 int main() {
-    constexpr auto selected = select_top<3>(Nums<10, 9, 8, 7, 11>{});
-    for (const int& n : selected) {
-        std::cout << n << " "; // 11 10 9
-    } 
+  constexpr auto selected = select_top<3>(Nums<10, 9, 8, 7, 11>{});
+  for (const int &n : selected) {
+    std::cout << n << " "; // 11 10 9
+  }
 }
